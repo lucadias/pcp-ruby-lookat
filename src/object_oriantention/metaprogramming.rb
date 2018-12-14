fc33b087db58d5e4c4aff9ecd82cf788ecec067e
Paiva Dias Luca tapaivad <luca.paivadias@stud.hslu.ch>
2018-12-14 09:04:38 +0100

    vererbung und metaprogramming

--- src/object_oriantention/metaprogramming.rb
+++ src/object_oriantention/metaprogramming.rb
@@ -4,12 +4,9 @@ class Class
     end
 end
 
-def Class.superhid()
-    "hi"
-end
 
-puts 4.class
 
+puts Class.superclass.superclass
 class Rechner
     def add2nums(var1, var2)
         var1.+(var2)
--- src/object_oriantention/vererbung.rb
+++ src/object_oriantention/vererbung.rb
@@ -1,1 +1,23 @@
-class 
+class Plant
+    def initialize(name)
+        @name = name
+    end
+end
+
+class Tree < Plant
+    def initialize(name)
+        super(name)
+        @has_trunk = true
+    end
+    def get_name()
+        @name
+    end
+    def get_has_trunk()
+        @has_trunk
+    end
+end
+
+birke = Tree.new("birke")
+
+puts birke.get_name
+puts birke.get_has_trunk
